a5565b3d92e58574f87f6ad37afeb4e01fc4a2a9
KYogesh20 <yogesh201201@gmail.com>
2023-02-23 16:25:47 +0530

    vehicles assignment done

---
+++ src/main/java/com/driver/boat.java
@@ -0,0 +1,33 @@
+package com.driver;
+
+public class boat implements WaterVehicle{
+
+    private String name;
+    private int capacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
